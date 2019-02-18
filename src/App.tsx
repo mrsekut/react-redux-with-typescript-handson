26fa0391bb01b1804b9ac769a7f545f1891234cb
mrsekut <k.cloudspider@gmail.com>
2019-02-19 01:36:35 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
